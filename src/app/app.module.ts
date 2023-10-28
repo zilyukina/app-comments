4c08955b03a69ddf8b2bf91645e24ad1fb6b8eff
Lilya Zilyukina <zilyukina.liliya@gmail.com>
2023-10-28 11:15:29 +0300

    feat: Firebase test connection

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SharedModule } from './shared/shared.module';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 
 @NgModule({
   declarations: [
     AppComponent,
   ],
-  imports: [BrowserModule, AppRoutingModule, SharedModule],
+  imports: [BrowserModule, AppRoutingModule, SharedModule, provideFirebaseApp(() => initializeApp({"projectId":"app-comments-f399a","appId":"1:331952555035:web:e5b1662d6f8337253364be","storageBucket":"app-comments-f399a.appspot.com","apiKey":"AIzaSyAyJeubnNoxzrQsyOHEU5RyqAT1Q92q-lE","authDomain":"app-comments-f399a.firebaseapp.com","messagingSenderId":"331952555035","measurementId":"G-6LYTTFE528"})), provideAuth(() => getAuth()), provideFirestore(() => getFirestore())],
   providers: [],
   bootstrap: [AppComponent],
 })
--- src/app/portal/components/comments/comments.component.ts
+++ src/app/portal/components/comments/comments.component.ts
@@ -1,7 +1,8 @@
 import { Component } from '@angular/core';
 import { ICommment } from '../../model/comment';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-
+import { Firestore, collection, collectionData } from '@angular/fire/firestore';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-comments',
@@ -9,25 +10,25 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
   styleUrls: ['./comments.component.scss']
 })
 export class CommentsComponent {
-  comments: ICommment[] = [
-    {
-      author: {
-        login: 'tolstokojev',
-        name: 'Толстокожев Вячеслав Филиппович'
-      },
-      text: 'Разнообразный и богатый опыт консультация с широким активом обеспечивает широкому кругу'
-    },
-    {
-      author: {
-        login: 'tolstokojev',
-        name: 'Толстокожев Вячеслав Филиппович'
-      },
-      text: 'Разнообразный и богатый опыт консультация с широким активом обеспечивает широкому кругу'
-    }
-  ];
+  comments!: Observable<any> ;// ICommment[] = [
+  //   {
+  //     author: {
+  //       login: 'tolstokojev',
+  //       name: 'Толстокожев Вячеслав Филиппович'
+  //     },
+  //     text: 'Разнообразный и богатый опыт консультация с широким активом обеспечивает широкому кругу'
+  //   },
+  //   {
+  //     author: {
+  //       login: 'tolstokojev',
+  //       name: 'Толстокожев Вячеслав Филиппович'
+  //     },
+  //     text: 'Разнообразный и богатый опыт консультация с широким активом обеспечивает широкому кругу'
+  //   }
+  // ];
   form: FormGroup;
 
-  constructor(private _fb: FormBuilder) {
+  constructor(private _fb: FormBuilder, private _fs: Firestore) {
     this.form = this._fb.group({
       text: ['', Validators.required],
     });
@@ -35,5 +36,11 @@ export class CommentsComponent {
 
   onSubmit(): void {
     this.form.reset();
+    const comments = collection(this._fs, 'comments');
+    this.comments= collectionData(comments, {idField: 'id'});
+    console.log(this.comments);
+    this.comments.subscribe((data: any)=> {
+      console.log(data);
+    });
   }
 }
